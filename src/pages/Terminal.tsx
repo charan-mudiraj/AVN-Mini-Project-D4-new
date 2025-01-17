53cb36a39f807838e539e6289c80f195332ba6c5
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2025-01-18 02:41:01 +0530

    .

--- src/pages/Terminal.tsx
+++ src/pages/Terminal.tsx
@@ -1,25 +1,147 @@
 import { useEffect } from "react";
-import {
-  getQueryResults,
-  jsonToTable,
-} from "../utils";
+import { getQueryResults, jsonToTable } from "../utils";
 import { Terminal as PRTerminal } from "primereact/terminal";
 import { TerminalService } from "primereact/terminalservice";
 
 export default function Terminal() {
-  const commandHandler = async (
-    command: string
-  ) => {
+  const commandHandler = async (command: string) => {
     let response;
 
-    if (command === "clear") {
+    if (command.toLowerCase().trim() === "clear") {
       TerminalService.emit("clear");
       return;
     }
 
-    const results = await getQueryResults(
-      command
-    );
+    if (command.toLowerCase().trim() === "help") {
+      TerminalService.emit(
+        "response",
+        jsonToTable([
+          {
+            command: "clear",
+            description: "Clears the terminal.",
+          },
+          {
+            command: "show project info",
+            description: "Displays info about the project.",
+          },
+          {
+            command: "show team",
+            description: "Displays team members of the project.",
+          },
+          {
+            command: "show code",
+            description: "Provided links to code repositories of this Project.",
+          },
+          {
+            command: "show stack",
+            description:
+              "Displays the list of web technologies used on the project.",
+          },
+        ])
+      );
+      return;
+    }
+
+    if (command.toLowerCase().trim() === "show project info") {
+      TerminalService.emit(
+        "response",
+        <p className="text-sky-500">
+          {`The Student Information Management System (SIMS) is an innovative and 
+comprehensive software solution designed to streamline and enhance the 
+management of student data for educational institutions. This system is 
+tailored to meet the needs of administrators, educators, and support 
+staff by providing a centralized platform for efficient storage, 
+retrieval, and analysis of student information, academic records, 
+attendance data, and related administrative tasks.`}
+        </p>
+      );
+      return;
+    }
+
+    if (command.toLowerCase().trim() === "show team") {
+      TerminalService.emit(
+        "response",
+        jsonToTable([
+          {
+            name: "Narsimha Reddy",
+            ht_no: "215U1A6711",
+          },
+          {
+            name: "Charan Midiraj",
+            ht_no: "215U1A6750",
+          },
+          {
+            name: "Akshay Kumar",
+            ht_no: "215U1A6718",
+          },
+          {
+            name: "Vijay",
+            ht_no: "215U1A6715",
+          },
+        ])
+      );
+      return;
+    }
+
+    if (command.toLowerCase().trim() === "show code") {
+      TerminalService.emit(
+        "response",
+        <>
+          <a
+            href="https://github.com/charan-mudiraj/AVN-Mini-Project-D4-new"
+            target="_blank"
+            className="hover:underline"
+          >
+            {"https://github.com/charan-mudiraj/AVN-Mini-Project-D4-new"}
+          </a>
+          <br />
+          <a
+            href="https://github.com/charan-mudiraj/AVN-Mini-Project-D4-Backend"
+            target="_blank"
+            className="hover:underline"
+          >
+            {"https://github.com/charan-mudiraj/AVN-Mini-Project-D4-Backend"}
+          </a>
+        </>
+      );
+      return;
+    }
+
+    if (command.toLowerCase().trim() === "show stack") {
+      TerminalService.emit(
+        "response",
+        jsonToTable([
+          {
+            frontend: "ReactJS",
+            backend: "NodeJS",
+            hosting: "AWS EC2",
+          },
+          {
+            frontend: "React Router",
+            backend: "ExpressJS",
+            hosting: "Nginx Server",
+          },
+          {
+            frontend: "Tailwind CSS",
+            backend: "mysql2",
+            hosting: "",
+          },
+          {
+            frontend: "Prime React",
+            backend: "",
+            hosting: "",
+          },
+          {
+            frontend: "ChartJS",
+            backend: "",
+            hosting: "",
+          },
+        ])
+      );
+      return;
+    }
+
+    const results = await getQueryResults(command);
 
     if (results.error) {
       response = (
@@ -44,17 +166,13 @@ export default function Terminal() {
       response = jsonToTable(results.data);
     }
 
-    if (response)
-      TerminalService.emit("response", response);
+    if (response) TerminalService.emit("response", response);
   };
   useEffect(() => {
     TerminalService.on("command", commandHandler);
 
     return () => {
-      TerminalService.off(
-        "command",
-        commandHandler
-      );
+      TerminalService.off("command", commandHandler);
     };
   }, []);
   return (
--- src/utils.ts
+++ src/utils.ts
@@ -5,18 +5,11 @@ export function capitalize(str: string) {
   if (!str) return str;
   return str
     .replace(/([a-z])([A-Z])/g, "$1 $2")
-    .replace(
-      /[_ ]([a-z])/g,
-      (_, p1) => " " + p1.toUpperCase()
-    )
-    .replace(/^([a-z])/g, (p1) =>
-      p1.toUpperCase()
-    );
+    .replace(/[_ ]([a-z])/g, (_, p1) => " " + p1.toUpperCase())
+    .replace(/^([a-z])/g, (p1) => p1.toUpperCase());
 }
 
-export const getQueryResults = async (
-  query: string
-) => {
+export const getQueryResults = async (query: string) => {
   const queryTemp: Query = {
     query,
     loading: true,
@@ -27,9 +20,7 @@ export const getQueryResults = async (
   let error: string = "";
   try {
     const res = await fetch(
-      `${
-        import.meta.env.VITE_API_STUDENTS
-      }/?exec=${query}`,
+      `${import.meta.env.VITE_API_STUDENTS}/?exec=${query}`,
       {
         method: "GET",
         headers: {
@@ -60,52 +51,31 @@ export function jsonToTable(jsonObject: any) {
   const keys = Object.keys(jsonObject[0]);
   const rows = jsonObject.map((obj: any) =>
     keys.map((key) =>
-      typeof obj[key] === "object"
-        ? JSON.stringify(obj[key])
-        : String(obj[key])
+      typeof obj[key] === "object" ? JSON.stringify(obj[key]) : String(obj[key])
     )
   );
 
   const columnWidths = keys.map((key, index) =>
-    Math.max(
-      key.length,
-      ...rows.map((row: any) => row[index].length)
-    )
+    Math.max(key.length, ...rows.map((row: any) => row[index].length))
   );
 
   const createLine = (char: any, joint = "+") =>
     joint +
-    columnWidths
-      .map((width) => char.repeat(width + 2))
-      .join(joint) +
+    columnWidths.map((width) => char.repeat(width + 2)).join(joint) +
     joint;
 
-  const createRow = (
-    data: any,
-    separator = "|"
-  ) =>
+  const createRow = (data: any, separator = "|") =>
     separator +
     data
-      .map(
-        (value: any, index: any) =>
-          ` ${value.padEnd(columnWidths[index])} `
-      )
+      .map((value: any, index: any) => ` ${value.padEnd(columnWidths[index])} `)
       .join(separator) +
     separator;
 
   const topLine = createLine("-");
   const header = createRow(keys);
   const separatorLine = createLine("-");
-  const body = rows
-    .map((row: any) => createRow(row))
-    .join("\n");
+  const body = rows.map((row: any) => createRow(row)).join("\n");
   const bottomLine = createLine("-");
 
-  return [
-    topLine,
-    header,
-    separatorLine,
-    body,
-    bottomLine,
-  ].join("\n");
+  return [topLine, header, separatorLine, body, bottomLine].join("\n");
 }
