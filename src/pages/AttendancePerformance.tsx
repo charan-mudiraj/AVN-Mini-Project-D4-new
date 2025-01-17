be0e177ae641dc95c5dbef290f8632d4a644e0f3
Charan-Mudiraj <r.charan.m.gm@gmail.com>
2025-01-18 01:39:19 +0530

    .

--- src/pages/AttendancePerformance.tsx
+++ src/pages/AttendancePerformance.tsx
@@ -4,6 +4,9 @@ import { Button } from "../components/ui/button";
 import { useRef, useState } from "react";
 import { singleStudentDetailsLoader } from "./SingleStudentDetails";
 import { Chart } from "primereact/chart";
+import { InputText } from "primereact/inputtext";
+import { Message } from "primereact/message";
+import { Toast } from "primereact/toast";
 
 const fields: (keyof StudentDetail)[] = ["ht_no", "name"];
 
@@ -163,43 +166,73 @@ const ProfileCard = ({ data }: { data: StudentDetail }) => {
 
 function HallTicketInputCard({
   onGetAtt,
+  loading,
 }: {
   onGetAtt: (num: string) => void;
+  loading: boolean;
 }) {
   const [value, setValue] = useState<string>("");
+  const toastRef = useRef<Toast>(null);
   return (
-    <form
-      className="rounded-xl border-[1px] border-[#464646] p-5 flex flex-col gap-3  w-[22rem] bg-gradient-to-r to-[#374151] from-[#1f2937]"
-      onSubmit={(e) => {
-        console.log("sdf");
-        e.preventDefault();
-        onGetAtt(value);
-      }}
-    >
-      <p>Enter Hall Ticket Number: </p>
-      <input
-        className="bg-transparent border-[1px] border-white py-1 px-2 rounded-md"
-        maxLength={10}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <Button type="submit">Get Attendance</Button>
-    </form>
+    <>
+      <Toast ref={toastRef} />
+      <form
+        className="rounded-xl border-[1px] border-[#464646] p-5 flex flex-col gap-3  w-[22rem] bg-gradient-to-r to-[#374151] from-[#1f2937]"
+        onSubmit={(e) => {
+          console.log("sdf");
+          e.preventDefault();
+          if (value.length < 10) {
+            toastRef.current?.show({
+              severity: "error",
+              summary: "Hall Ticket Number should be of 10 digits length",
+            });
+            return;
+          }
+          onGetAtt(value);
+        }}
+      >
+        <p>Enter Hall Ticket Number: </p>
+        <InputText
+          className="px-2 py-1"
+          disabled={loading}
+          value={value}
+          maxLength={10}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button type="submit" disabled={loading}>
+          {loading ? "loading..." : "Get Attendance"}
+        </Button>
+      </form>
+    </>
   );
 }
 
 export default function AttendancePerformance() {
-  const [details, setDetails] = useState<StudentDetail | null>(null);
+  const [details, setDetails] = useState<StudentDetail | undefined | null>(
+    undefined
+  );
+  const [loading, setLoading] = useState<boolean>(false);
   const onGetAtt = async (num: string) => {
+    setLoading(true);
     const data = await singleStudentDetailsLoader({
       params: { ht_no: num },
     } as any);
+    setLoading(false);
+    if (!data) {
+      setDetails(null);
+      return;
+    }
     setDetails(data);
   };
   return (
     <div className="flex items-center justify-center py-10 flex-col gap-5">
-      <HallTicketInputCard onGetAtt={onGetAtt} />
-      {details && <ProfileCard data={details} />}
+      <HallTicketInputCard onGetAtt={onGetAtt} loading={loading} />
+      {details !== undefined &&
+        (details === null ? (
+          <Message severity="error" text="Invalid Hall Ticket Number" />
+        ) : (
+          <ProfileCard data={details} />
+        ))}
     </div>
   );
 }
